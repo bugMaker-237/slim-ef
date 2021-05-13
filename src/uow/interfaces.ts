44fc1cb0d8579ebf172bd150a2b5844b27327069
Etienne Yamsi <etienne.y@glotelhomail.com>
2021-05-13 13:51:08 +0100

    #Hot: Fixing IDbContext interface

--- src/uow/interfaces.ts
+++ src/uow/interfaces.ts
@@ -65,7 +65,7 @@ export interface IDbContext {
    * This method will automatically call DetectChanges() to discover any changes to
    * entity instances before saving to the underlying database.
    */
-  saveChanges(): void;
+  saveChanges(): Promise<ISavedTransaction>;
   /**
    * Releases the allocated resources for this context.
    */
